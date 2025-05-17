6406b17a86e9da9bf0fad3836119337c68088686
Burak Altun <52549565+mburakaltun@users.noreply.github.com>
2025-05-17 21:16:44 +0300

    initialize mba-spring-core project with Maven and Spring Boot setup

---
+++ src/main/java/com/mburakaltun/mbaspringcore/MbaSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mburakaltun.mbaspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbaSpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbaSpringCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mburakaltun/mbaspringcore/MbaSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mburakaltun.mbaspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbaSpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
